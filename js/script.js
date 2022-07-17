6371bc3b25c177c89e3524abbd65ac5a89cfcd30
Ali Bahadoran <alibahadoran.eng@gmail.com>
2022-07-17 20:31:38 +0430

    End Project

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const bg = document.querySelector(".bg");
+const span = document.querySelectorAll(".text span");
+const text = document.querySelector(".text");
+const line = document.querySelector("#line");
+
+span.forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+        const id = item.getAttribute("data-id");
+        const bgEl = bg.querySelector(`.id-${id}`);
+
+        bg.querySelectorAll("img").forEach((img) => {
+            img.style.display = "none";
+        });
+        bgEl.style.display = "block";
+        bgEl.style.animation = "animate 1s ease forwards";
+
+        const card = item.getBoundingClientRect();
+        const cardText = text.getBoundingClientRect();
+
+        line.style.width = card.width + "px";
+        line.style.left = card.left - cardText.left + "px";
+    });
+});
